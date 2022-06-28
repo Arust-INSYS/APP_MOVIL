ea596add461a60b0d193d6f182305646fea8f0d2
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2022-06-27 22:38:34 -0500

    Creación proyecto

---
+++ proyecto_arust/app/src/main/java/com/app_movil/proyecto_arust/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app_movil.proyecto_arust;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
